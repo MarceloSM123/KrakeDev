c8ccf1b01612973f48043035928a29cef4340f94
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-18 11:57:49 -0500

    modificar la imagen

--- EjercicioGit/Ejercicio.js
+++ EjercicioGit/Ejercicio.js
@@ -1,3 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/yagane-gane.gif"
     console.log("Ingresa a saludar");
+
 }
