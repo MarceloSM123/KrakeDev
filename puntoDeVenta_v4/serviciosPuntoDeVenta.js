117044cf9139f6891047a19f25853f6c5b5c5fc9
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-19 12:49:02 -0500

    implementacion de mostrar subtotal con sus respectivas funciones

---
+++ puntoDeVenta_v4/serviciosPuntoDeVenta.js
@@ -0,0 +1,19 @@
+calcularValorDescuento=function(monto, porcentajeDeDescuento){
+    let descuento = monto*(porcentajeDeDescuento/100);
+    return descuento
+}
+
+calcularIva=function(monto){
+    let iva = calcularValorDescuento(monto, 12);
+    return iva;
+}
+
+calcularSubtotal=function(precio,cantidad){
+    let subtotal=precio*cantidad;
+    return subtotal;
+}
+
+calcularTotal=function(subtotal,descuento,iva){
+    let valorTotal=subtotal-descuneto+iva;
+    return valorTotal;
+}
