0a7c5f48e1a1d071ba8acaca22cea323ddc24ea3
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-19 12:23:32 -0500

    se separa archivo utilitarios.js

--- Utilitarios/Saludo.js
+++ Utilitarios/Saludo.js
@@ -14,39 +14,3 @@ Saludar=function(){
     mostrarTextoEnCaja("txtNombre", "");
 }
 
-recuperarInt=function(idcomponente){
-    let valorCaja=RecuperarTexto(idcomponente);
-    let valorEntero=parseInt(valorCaja);
-    return valorEntero;
-}
-
-recuperarFloat=function(idcomponente){
-    let valorCaja=RecuperarTexto(idcomponente);
-    let valorFlotante=parseFloat(valorCaja);
-    return valorFlotante;
-}
-
-RecuperarTexto=function(idComponente){
-    let componente;
-    let valorIngresado;
-    componente=document.getElementById(idComponente);
-    valorIngresado=componente.value;
-return valorIngresado
-}
-
-mostrarTexto=function(idComponente, mensaje){
-let componente;
-componente=document.getElementById(idComponente);
-componente.innerText=mensaje;
-}
-
-mostrarImagen=function(idComponente, rutaImagen){
-    let componente =document.getElementById(idComponente);
-    componente.src=rutaImagen;
-}
-
-mostrarTextoEnCaja=function(idComponente, mensaje){
-let componente;
-componente=document.getElementById(idComponente);
-componente.value=mensaje;
-}
