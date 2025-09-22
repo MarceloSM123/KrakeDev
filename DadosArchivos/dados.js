eff3f19aa0552267dcf1bf31b8b61ea282197617
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-21 19:40:00 -0500

    implementacion funcion lanzar dado

--- DadosArchivos/dados.js
+++ DadosArchivos/dados.js
@@ -1,4 +1,14 @@
 jugar=function(){   
+    lanzarDado();
+}
+// crear una funcion llamada lanzar dados
+//no tiene parametros
+// retorna un numero del 1 al 6
+
+lanzarDado=function(){
     let aleatorio=Math.random();
-    console.log(aleatorio);
+    let numeroMultiplicado=aleatorio*6;
+    let numeroEntero=parseInt(numeroMultiplicado);
+    let valorDado=numeroEntero+1;
+    return valorDado
 }
