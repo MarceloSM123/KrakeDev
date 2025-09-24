d88b1efa76a3e2175af0b62b1b2488e5fe35ba21
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-24 13:41:22 -0500

    codigo organizado

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,16 +5,16 @@ calcularPromedioNotas=function(){
     let promedio=calcularPromedio(nota1,nota2,nota3);
     cambiarTexto("lblpromedio","PROMEDIO: "+promedio.toFixed(2));
     if(promedio<5 && promedio>0){
-cambiarImagen("cmpImagen","./imagen/fracaso.gif");
-cambiarTexto("cmpMensaje","REPROBADO");
+        cambiarImagen("cmpImagen","./imagen/fracaso.gif");
+        cambiarTexto("cmpMensaje","REPROBADO");
     }else if(promedio>=5 && promedio<=8){
-cambiarImagen("cmpImagen","./imagen/buen-trabajo-good-job.gif");
-cambiarTexto("cmpMensaje","BUEN TRABANJO");
+        cambiarImagen("cmpImagen","./imagen/buen-trabajo-good-job.gif");
+        cambiarTexto("cmpMensaje","BUEN TRABANJO");
     } else if(promedio>8 && promedio<=10){
-cambiarImagen("cmpImagen","./imagen/yagane-gane.gif");
-cambiarTexto("cmpMensaje","EXCELENTE");
+        cambiarImagen("cmpImagen","./imagen/yagane-gane.gif");
+        cambiarTexto("cmpMensaje","EXCELENTE");
     } else{
-cambiarImagen("cmpImagen","./imagen/error-warning-computer-glitch-8h17x99p10ev461u.gif");
-cambiarTexto("cmpMensaje","DATOS INCORRECTOS");
+        cambiarImagen("cmpImagen","./imagen/error-warning-computer-glitch-8h17x99p10ev461u.gif");
+        cambiarTexto("cmpMensaje","DATOS INCORRECTOS");
     }
 }
