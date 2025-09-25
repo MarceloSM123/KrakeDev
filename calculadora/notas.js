644553b088c5f7ee47cfc00663d24073323b3fb9
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-25 10:37:11 -0500

    implementacion de un segundo metodo de validacion alterno

--- calculadora/notas.js
+++ calculadora/notas.js
@@ -4,7 +4,7 @@ calcularPromedio = function (nota1, nota2, nota3) {
     return promedio;
 }
 
-calcular = function () {
+calcular1 = function () {
     let nota1;
     let nota2;
     let nota3;
@@ -29,4 +29,33 @@ calcular = function () {
         }
     }
     
+}
+
+calcular = function () {
+    let nota1;
+    let nota2;
+    let nota3;
+    let resultado;
+    let resultadoFormato;
+    let existeError= false;
+    nota1 = recuperarFloat("txtNota1");
+    if(isNaN(nota1)){
+        alert("ERROR");
+        existeError= true;
+    }
+    nota2 = recuperarFloat("txtNota2");
+    if(isNaN(nota2)){
+        alert("ERROR");
+        existeError= true;
+    }
+    nota3 = recuperarFloat("txtNota3");
+    if(isNaN(nota3)){
+        alert("ERROR");
+        existeError= true;
+    }
+    if(existeError==false){
+         resultado = calcularPromedio(nota1, nota2, nota3);
+        resultadoFormato = resultado.toFixed(2);
+         mostrarTexto("lblResultado", resultadoFormato);
+    }
 }
