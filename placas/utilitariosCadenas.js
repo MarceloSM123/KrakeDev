fa62ae585327bd3f2a2a6c05ac76948dfad58e07
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-26 10:54:17 -0500

    implementacion de funciones esMayuscula, esDigito, esGuion

---
+++ placas/utilitariosCadenas.js
@@ -0,0 +1,26 @@
+esMayuscula=function(caracter){
+    let codigo=caracter.charCodeAt(0);
+    if(codigo>=65 && codigo<=90){
+        return true
+    }else{
+        return false
+    }
+}
+
+esDigito=function(caracter){
+    let codigo=caracter.charCodeAt(0);
+    if(codigo>=48 && codigo<=57){
+        return true
+    }else{
+        return false
+    }
+}
+
+esGuion=function(caracter){
+    let codigo=caracter.charCodeAt(0);
+    if(codigo==45){
+        return true
+    }else{
+        return false
+    }
+}
