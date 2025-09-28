9367bfc348292bfafcf300c016cb447c7bf26863
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-28 11:05:23 -0500

    ejercicio 2 - sentencia for

--- bucles/ejemplosFor.js
+++ bucles/ejemplosFor.js
@@ -5,3 +5,11 @@ mostrarNumeros=function(){
     }
     console.log("despues del for");
 }
+
+mostrarNumeros1=function(){
+    console.log("antes del for");
+    for(let indice=0;indice<=5;indice++){
+        console.log(indice);
+    }
+    console.log("despues del for");
+}
