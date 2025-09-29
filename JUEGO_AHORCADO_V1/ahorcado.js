8f47c6a313a38d8bd1a7dbaef4408bb2bb5b940c
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-29 12:40:33 -0500

    implementacion de variables globales intentos, coincidencias y errores

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,5 +1,8 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
 let palabraSecreta;
+let intentos=0;
+let coincidencias=0;
+let errores=0;
 
 esMayuscula=function(caracter){
     let codigo=caracter.charCodeAt(0);
@@ -35,37 +38,51 @@ guardarPalabra=function(){
 mostrarLetra=function(letra, posicion){
 if(posicion==0){
     mostrarTexto("div0",letra);
+    coincidencias++;
 
 }
 if(posicion==1){
     mostrarTexto("div1",letra);
+    coincidencias++;
 }
 if(posicion==2){
     mostrarTexto("div2",letra);
+    coincidencias++;
 }
 if(posicion==3){
     mostrarTexto("div3",letra);
+    coincidencias++;
 }
 if(posicion==4){
     mostrarTexto("div4",letra);
+    coincidencias++;
 }
 }
 
 validar=function(letra){
-    let letrasEncontradas;
+    let letrasEncontradas=0;
     for(let i=0;i<=palabraSecreta.length;i++){
         if(letra==palabraSecreta.charAt(i)){
             letrasEncontradas++;
             mostrarLetra(letra,i);
         }
     }
+    if(letrasEncontradas==0){
+        alert("LA LETRA NO ES PARTE DE LA PALABRA");
+        errores++;
+    }
 }
 
 ingresarLetra=function(){
     let letra;
+        intentos++;
     letra=recuperarTexto("txtLetra");
     if(esMayuscula(letra)==true){
        validar(letra); 
+       if(concidencias==5){
+       alert("HA GANADO");}
+       if(intentos==10){
+       alert("HA PERDIDO");}
     } else{
         alert("SOLO SE ACEPTAN MAYUSCULAS");
     }
