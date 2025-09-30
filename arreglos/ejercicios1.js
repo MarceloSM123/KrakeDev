4eb677c215e2911b6d9e3d46c7eefd4be3555f00
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-09-29 20:02:29 -0500

    implementacion del boton agrgegar nota y relacionado con la funcion probarAgregar/ ARREGLOS PARTE3

--- arreglos/ejercicios1.js
+++ arreglos/ejercicios1.js
@@ -1,6 +1,17 @@
-let notas=[];
+let notas=[];// la variable global no se reinicia hasta que se reincie la pagina
 agregarElementos=function(){
+    // let notas=[]; como variable local de la funcion se reincia cada vez que se utiliza la funcion
     notas.push(8);
     notas.push(9);
-    console.log(notas.length(notas));
+    console.log(notas.length);
+}
+
+agregarNotas=function(nota){
+    notas.push(nota);
+}
+
+probarAgregar=function(){
+    let notaRecuperada;
+    notaRecuperada=recuperarInt("txtNota");
+    agregarNotas(notaRecuperada);
 }
