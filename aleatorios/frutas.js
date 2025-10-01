316fa5fea523b95ca428353a4acf3d1913f8bad5
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-10-01 11:14:14 -0500

    ejercicio sentencia brak

---
+++ aleatorios/frutas.js
@@ -0,0 +1,22 @@
+let frutas=["pera","manzana","banana"];
+probarBusqueda=function(){
+    let frutaIngresada=recuperarTexto("lblFruta");
+    let resultado=buscar(frutaIngresada);
+   if(resultado==null){
+    alert("no existe la fruta");
+    }else{
+        alert(frutaIngresada+" existe en el cesto");
+    }
+}
+buscar=function(fruta){
+    let elemento;
+    let frutaEncontrada=null;
+    for(let i=0; i<frutas.length; i++){
+        elemento=frutas[i];
+        if(frutas==elemento){
+            frutaEncontrada=elemento;
+            break;
+        }
+        return frutaEncontrada;
+    }
+}
