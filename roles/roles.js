e31b9d53d661612b63085b1467a1826baa61d044
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-10-03 11:46:36 -0500

    implementacion de la funcion mostrar empleados/ tabla/ RETO 46

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,14 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1237897410",nombre:"Marcelo",apellido:"Salcedo",sueldo:1000.0}
 ]
 
 mostrarOpcionEmpleado=function(){
     mostrarComponente("divEmpleado");
     ocultarComponente("divRol");
     ocultarComponente("divResumen");
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol=function(){
@@ -19,4 +21,23 @@ mostrarOpcionResumen=function(){
     mostrarComponente("divResumen");
     ocultarComponente("divEmpleado");
     ocultarComponente("divRol");
+}
+
+mostrarEmpleados=function(){
+    let cmpTablaEmpleados=document.getElementById("tablaEmpleados");
+   let elementoRecuperado;
+   let tabla="<table><tr><th> CEDULA </th><th> NOMBRE </th><th> APELLIDO </th><th> SUELDO </th></tr>"
+    for(let i=0;i<empleados.length;i++){
+        elementoRecuperado=empleados[i];
+        tabla+=
+        "<tr>"+
+        "<td>"+elementoRecuperado.cedula+"</td>"+
+        "<td>"+elementoRecuperado.nombre+"</td>"+
+        "<td>"+elementoRecuperado.apellido+"</td>"+
+        "<td>"+elementoRecuperado.sueldo+"</td>"+
+        "</tr>"
+    }
+
+tabla+="</table>";
+cmpTablaEmpleados.innerHTML=tabla;
 }
